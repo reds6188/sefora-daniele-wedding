94edb09b11fc70515b64c4a3a39c18d57ccd0c2f
reds6188 <reds6188@gmail.com>
2022-06-10 17:22:10 +0200

    Added countdown and navigation menu

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+window.onresize = function() {
+  if(screen.width < 400) {
+    document.getElementById("menu-button").classList.remove("hidden");
+  }
+  else {
+    document.getElementById("menu-button").classList.add("hidden");
+  }
+};
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -15,3 +24,42 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// Open when someone clicks on the span element
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+// Close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 3, 2022 11:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
